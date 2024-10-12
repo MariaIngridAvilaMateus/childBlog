14995a940128ee58df14baaebb246f6c90124b85
MariaAvila <mainama8@hotmail.com>
2024-10-12 20:51:11 +0200

    inicio proyecto

---
+++ child/index-view.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Paginacion</title>
+</head>
+<body>
+  <div class="contenedor">
+    <h1>Articulos</h1>
+    <section class="articulos">
+      <ul>
+        <li>1.- Lorem ipsum elit. Eum, ipsa? </li>
+        <li>1.- Lorem ipsum elit. Eum, ipsa? </li>
+        <li>1.- Lorem ipsum elit. Eum, ipsa? </li>
+        <li>1.- Lorem ipsum elit. Eum, ipsa? </li>        
+      </ul>
+    </section>
+
+    <section class="paginacion">
+      <ul>
+
+        <li class="disable"><a href="#">&laquo;</a></li>
+        <li class="active"><a href="#">1</a></li>
+        <li><a href="#">2</a></li>
+        <li><a href="#">3</a></li>
+        <li><a href="#">4</a></li>
+        <li><a href="#">&raquo;</a></li>
+      </ul>
+    </section>
+  </div>
+
+</body>
+</html>
